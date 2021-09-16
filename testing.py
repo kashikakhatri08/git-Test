d5fbcbde08cb6bb0ccef18a4c1c7cf6bf8cee472
Kashika <40196858+kashikakhatri08@users.noreply.github.com>
2021-09-16 20:01:33 +0530

    sdfghj
    dsfghjkl
    dfghjk
    dfghjkl

--- testing.py
+++ testing.py
@@ -231,10 +231,12 @@ error_ex["errror_key"]=error_list
 
 result_dict.update(error_ex)
 print(dict_ex)
-cmd = 'bat (script: 'git log -1 --pretty=full ${env.GIT_COMMIT}', returnStdout: true).trim()'
-process = subprocess.getoutput(cmd)
+lines = subprocess.check_output(
+        ['git', 'log'], stderr=subprocess.STDOUT
+    ).split('\n')
 
-print(process)
+
+print(lines)
 with open('data.json', 'w') as json_file:
     data = []
     json.dump(result_dict, json_file)
