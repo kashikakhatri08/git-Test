dc74cdae8fd99b1f1b25fc60a5d0efcc321efb9b
Kashika <40196858+kashikakhatri08@users.noreply.github.com>
2021-09-20 19:01:33 +0530

    hello
    hi

--- testing.py
+++ testing.py
@@ -241,8 +241,10 @@ stream = os.popen('git log')
 output = stream.read()
 # print(output)
 
-result = re.findall(r'.*' + 'CHANGELIST_local:.*' + '.*(?:.*\n){' + str(2) + '}.*', bld_console)
+
+result = re.findall(r'.*' + '(?<=CHANGELIST_local:).*' + '.*(?:.*\n){' + str(2) + '}.*', bld_console)
 print(result)
+
 with open('data.json', 'w') as json_file:
     data = []
     json.dump(result_dict, json_file)
