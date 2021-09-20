88b193748c66c95a25d981838c62a2099dcc9a8c
Kashika <40196858+kashikakhatri08@users.noreply.github.com>
2021-09-20 19:39:07 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -242,7 +242,7 @@ output = stream.read()
 # print(output)
 
 
-result = re.findall(r'.*' + '(?<=CHANGELIST_local:).*' + '.*(?:.*\n){' + str(2) + '}.*', bld_console)
+result = re.findall(r'.*' + '(?<=CHANGELIST_local:)' + '.*(?:.*)' + str(2) , bld_console)
 print(result)
 
 with open('data.json', 'w') as json_file:
