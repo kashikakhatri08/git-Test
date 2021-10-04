66742f42810c9cd6124727065ab939b021177809
Kashika <40196858+kashikakhatri08@users.noreply.github.com>
2021-10-04 13:41:35 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -46,11 +46,9 @@ job_name = os.environ['JOB_BASE_NAME']
 print("-------"+job_name)
 job_url = J[os.environ['JOB_NAME']]
 bld = job_url.get_build(PARENT_BUILD_NUMBER)
-job_api = os.environ['BUILD_URL']
-
-#+"/api/json"
-# rest_api = rest_api_call(job_api)
-print(job_api)
+job_api = os.environ['BUILD_URL'] +"api/json"
+rest_api = rest_api_call(job_api)
+print(rest_api)
 print("-----------------")
 # job_status = J[os.environ['currentBuild']]
 # job_status_second = J[os.environ['result']]
