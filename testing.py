e143de22bde2e9dfbf3828505495e5f426b77b68
Kashika <40196858+kashikakhatri08@users.noreply.github.com>
2021-10-04 13:39:30 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -46,7 +46,7 @@ job_name = os.environ['JOB_BASE_NAME']
 print("-------"+job_name)
 job_url = J[os.environ['JOB_NAME']]
 bld = job_url.get_build(PARENT_BUILD_NUMBER)
-job_api = J[os.environ['BUILD_URL']] 
+job_api = os.environ['BUILD_URL']
 
 #+"/api/json"
 # rest_api = rest_api_call(job_api)
